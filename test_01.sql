2f3c3e235d24fa1cd5ff8b1e224cb694a0a2850b
Nandan Hegde <nhegd01@safeway.com>
2026-07-06 21:06:44 +0530

    test: trigger claude review

--- test_01.sql
+++ test_01.sql
@@ -1,7 +1,1 @@
--- migrations/001_create_users.sql
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY,
-    FirstName VARCHAR(100),
-    Email_Address VARCHAR(255),
-    createdAt TIMESTAMP
-);
+select * from table;
